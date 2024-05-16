bbc5d1ce8f12b0cd384ca7e35d0663f88a0d9544
Sjoerd <113519405+DreojsNL@users.noreply.github.com>
2024-05-16 08:35:35 +0200

    Car

--- F1RaceSim/Assets/Script/CarController.cs
+++ F1RaceSim/Assets/Script/CarController.cs
@@ -1,56 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Xml;
 using TMPro;
-using UnityEditor.ShortcutManagement;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class CarController : MonoBehaviour
 {
-    public WheelCollider frontLeft;
-    public WheelCollider frontRight;
+    // Wheel colliders
     public WheelCollider backLeft;
     public WheelCollider backRight;
-    public Transform frontLeftM;
-    public Transform frontRightM;
+    public WheelCollider frontLeft;
+    public WheelCollider frontRight;
+
+    // Wheel transforms
     public Transform backLeftM;
     public Transform backRightM;
-    public TextMeshProUGUI rmpText;
-    public TextMeshProUGUI kmhText;
-    public TextMeshProUGUI gearText;
-    public TextMeshProUGUI engineStatus;
-    public AnimationCurve hpToRPMCurve;
-    private string currentRPMstring;
+    public Transform frontLeftM;
+    public Transform frontRightM;
 
+    // UI Texts
+    public TextMeshProUGUI engineStatus;
+    public TextMeshProUGUI gearText;
+    public TextMeshProUGUI kmhText;
+    public TextMeshProUGUI rmpText;
+    public TextMeshProUGUI tcText;
+
+    // Car parameters
+    public AnimationCurve hpToRPMCurve;
     public float acceleration = 500f;
     public float brakeForce = 300f;
+    public float differentialRatio;
+    public float idleRPM = 1000f;
+    public float maxRPM = 6000f;
     public float maxTurnAngle = 15;
 
-    // RPM related variables
-    public float maxRPM = 6000f; // Maximum RPM of the engine
-    public float idleRPM = 1000f;
-    private bool engineIsBroke;
-    private bool engineOn;
-    private float currentRPM = 0f;
-    public int currentGear = 1; // Current gear
-    public float[] gearRatios = { 3.5f, 2.5f, 1.8f, 1.4f };
-    public float differentialRatio;
-
-    public float clutch;
-    private float clutchInvers;
+    // Engine related variables
+    public bool engineIsBroke;
+    public bool engineOn;
+    public bool tractionControlOn = true; // Traction Control toggle
     private float gasInput;
+    private float clutch;
+    private float clutchInvers;
     private float currentAcceleration = 0f;
     private float currentBrakeForce = 0f;
+    private float currentRPM = 0f;
     private float currentTurnAngle = 0f;
+    public int currentGear = 1;
+    public float[] gearRatios = { 3.5f, 2.5f, 1.8f, 1.4f };
+
     private Rigidbody rb;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
+
     private void Update()
     {
+        if(tractionControlOn)
+        {
+            tcText.text = "TC:on";
+        }
+        else
+        {
+            tcText.text = "TC:off";
+        }
         if (engineOn)
         {
             engineStatus.text = "Engine Status:on";
@@ -63,89 +74,114 @@ public class CarController : MonoBehaviour
         {
             engineStatus.text = "Engine Status:broken";
         }
-        if (Input.GetKeyDown(KeyCode.I)) // startCar
+        if (Input.GetKeyDown(KeyCode.I) && clutch == 2) // startCar
         {
-        engineOn = !engineOn;
+            engineOn = !engineOn;
         }
+
         if (!engineOn)
         {
-            currentRPM = 0f;
+            currentRPM = Mathf.Lerp(currentRPM, 0f, 30 * Time.deltaTime);
         }
+
         gearText.text = currentGear.ToString();
+
         clutchInvers = Input.GetAxis("Clutch");
         clutch = 1f - clutchInvers;
-        kmhText.text = "KMH:" + (rb.velocity.magnitude * 3.6f).ToString(format:"000");
+
+        kmhText.text = "KMH:" + (rb.velocity.magnitude * 3.6f).ToString(format: "000");
+
+        int shiftRandom = Random.Range(1000, 2500);
         if (Input.GetKeyDown(KeyCode.LeftShift) && clutch == 2) // Shift up
         {
             currentGear++;
-            if(!engineIsBroke)
+            if (!engineIsBroke)
             {
-                currentRPM = currentRPM - 5000;
+                currentRPM = currentRPM - shiftRandom;
             }
-            
         }
         else if (Input.GetKeyDown(KeyCode.LeftControl) && currentGear > 1 && clutch == 2) // Shift down
         {
             currentGear--;
             if (!engineIsBroke)
             {
-                currentRPM = currentRPM + 5000;
+                currentRPM = currentRPM + shiftRandom;
             }
         }
+
+        // Toggle Traction Control
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            tractionControlOn = !tractionControlOn;
+        }
     }
+
     private void FixedUpdate()
     {
-        currentRPMstring = currentRPM.ToString(format: "0000");
+        string currentRPMstring = currentRPM.ToString(format: "0000");
         rmpText.text = "RPM:" + currentRPMstring;
+
         // Control steering
         currentTurnAngle = maxTurnAngle * Input.GetAxis("Horizontal");
         frontRight.steerAngle = currentTurnAngle;
         frontLeft.steerAngle = currentTurnAngle;
 
-        if (!engineIsBroke || !engineOn)
+        if (!engineIsBroke && engineOn)
         {
-        gasInput = Input.GetAxis("Vertical");
-        if(clutch == 1)
-        {
-            int random = Random.Range(-50, 50);
-            float wheelRPM = Mathf.Abs((backRight.rpm + backLeft.rpm) / 2f) * gearRatios[currentGear] * differentialRatio;
-            currentRPM = Mathf.Lerp(currentRPM, Mathf.Max(idleRPM - 100, wheelRPM), Time.deltaTime * 3f);
-            currentAcceleration = (hpToRPMCurve.Evaluate(currentRPM / maxRPM) * acceleration / currentRPM) * gearRatios[currentGear] * differentialRatio * 5252f * clutch;
-        }
-        else
-        {
-            int random = Random.Range(-50, 50);
-            currentRPM = Mathf.Lerp(currentRPM, Mathf.Max(idleRPM, maxRPM * gasInput) + random, Time.deltaTime);
-            currentAcceleration = 0f;
-        }
-        if (currentRPM < maxRPM && currentGear != 0 && engineOn)
-        {
-            backLeft.motorTorque = currentAcceleration * gasInput;
-            backRight.motorTorque = currentAcceleration * gasInput;
-        }
-        else
-        {
-            backLeft.motorTorque = 0f;
-            backRight.motorTorque = 0f;
-        }
-        if(gasInput == -1 )
-        {
-            currentBrakeForce = brakeForce;
-        }
-        if(currentRPM < 1000 && clutch == 1)
+            gasInput = Input.GetAxis("Vertical");
+
+            if (clutch == 1)
+            {
+                int random = Random.Range(-50, 50);
+                float wheelRPM = Mathf.Abs((backRight.rpm + backLeft.rpm) / 2f) * gearRatios[currentGear] * differentialRatio;
+                currentRPM = Mathf.Lerp(currentRPM, Mathf.Max(idleRPM - 100, wheelRPM), Time.deltaTime * 3f);
+                currentAcceleration = (hpToRPMCurve.Evaluate(currentRPM / maxRPM) * acceleration / currentRPM) * gearRatios[currentGear] * differentialRatio * 5252f * clutch;
+            }
+            else
+            {
+                int random = Random.Range(-50, 50);
+                currentRPM = Mathf.Lerp(currentRPM, Mathf.Max(idleRPM, maxRPM / 2 * gasInput) + random, Time.deltaTime);
+                currentAcceleration = 0f;
+            }
+            if (currentRPM < maxRPM && currentGear != 0 && engineOn)
+            {
+                // Apply traction control if enabled
+                if (tractionControlOn)
+                {
+                    // Adjust gasInput based on current RPM
+                    gasInput *= Mathf.Clamp01((maxRPM - currentRPM) / maxRPM);
+                }
+
+                backLeft.motorTorque = currentAcceleration * gasInput;
+                backRight.motorTorque = currentAcceleration * gasInput;
+            }
+            else
+            {
+                backLeft.motorTorque = 0f;
+                backRight.motorTorque = 0f;
+            }
+
+            if (gasInput == -1)
+            {
+                currentBrakeForce = brakeForce;
+            }
+
+            if (currentRPM < 1000 && clutch == 1)
             {
                 EngineStall();
             }
-        if(currentRPM > 10000)
-        {
-            currentBrakeForce = 3000;
-            EngineBroke();
-        }
-        if(currentRPM < 0)
+
+            if (currentRPM > maxRPM && gasInput == 0)
+            {
+                EngineBroke();
+            }
+
+            if (currentRPM < 0)
             {
                 EngineStall();
             }
         }
+
         frontRight.brakeTorque = currentBrakeForce;
         frontLeft.brakeTorque = currentBrakeForce;
         backLeft.brakeTorque = currentBrakeForce;
@@ -156,11 +192,9 @@ public class CarController : MonoBehaviour
         UpdateWheel(frontRight, frontRightM);
         UpdateWheel(backLeft, backLeftM);
         UpdateWheel(backRight, backRightM);
-
-        
     }
 
-    void UpdateWheel(WheelCollider col, Transform trans)
+    private void UpdateWheel(WheelCollider col, Transform trans)
     {
         Vector3 position;
         Quaternion rotation;
@@ -169,14 +203,16 @@ public class CarController : MonoBehaviour
         trans.position = position;
         trans.rotation = rotation;
     }
-    void EngineBroke()
+
+    private void EngineBroke()
     {
-        currentRPM = 0f;
+        currentBrakeForce = 3000;
+        engineOn = false;
         engineIsBroke = true;
     }
-    void EngineStall()
+
+    private void EngineStall()
     {
-        currentRPM = 0f;
         engineOn = false;
     }
 }
